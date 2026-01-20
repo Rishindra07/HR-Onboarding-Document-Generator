98521848583cfa46d214227d74f6fd8eb4e72913
rishindra.k15@example.com <Rishindra@example.com>
2026-01-21 02:40:51 +0530

    updated-cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", // local frontend
   "http://localhost:3000", // alternative local frontend
-  "https://hr-onboarding-document-generator.vercel.app" // production frontend
+  "https://hr-onboarding-document-generator.vercel.app",
+  "https://hr-onboarding-document-generator.vercel.app/"// production frontend
 ];
 
 app.use(cors({
