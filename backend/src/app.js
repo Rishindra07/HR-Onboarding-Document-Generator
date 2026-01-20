abd017c69eb3ffd35caa412cdc1c2d7470a56746
rishindra.k15@example.com <Rishindra@example.com>
2026-01-20 21:07:15 +0530

    updated-services

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,3 +2,34 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
+
+import templateRoutes from "./routes/templateRoute.js";
+import clauseRoutes from "./routes/clauseRoutes.js";
+import generateRoute from "./routes/generateRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // parse JSON bodies
+
+// Connect to MongoDB
+connectDB();
+
+// Register Routes
+app.use("/api/templates", templateRoutes);
+app.use("/api/clauses", clauseRoutes);
+app.use("/api/generate", generateRoute);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("HR Onboarding Document Generator API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
